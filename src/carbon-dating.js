12cbb630d63619329106f31f1312d25c22aa1013
Vlada-only-one <vlada.only.one@gmail.com>
2021-03-30 16:46:52 +0300

    fix: fix solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,9 +5,7 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity !== "string" ||
-    isNaN(parseFloat(sampleActivity)) ||
-    sampleActivity > MODERN_ACTIVITY ||
-    sampleActivity <= 0) {
+    isNaN(parseFloat(sampleActivity)) || sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0) {
     return false;
   } else {
     const result = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
